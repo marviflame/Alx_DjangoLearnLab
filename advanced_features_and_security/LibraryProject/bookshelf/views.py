72fc207361e8784e85fd7c34cdb160f194b8d187
marviflame <chalcedony800@gmail.com>
2024-09-01 15:27:49 +0100

    updated

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,43 +1,70 @@
-from django.shortcuts import render, redirect, get_list_or_404
-from django.contrib.auth.decorators import login_required, permission_required
-from django.contrib.auth.models import Group
+from django.contrib.auth.decorators import permission_required
+from django.db.models import Q
+from django.shortcuts import redirect
+from django.shortcuts import render, get_object_or_404
+
+from .forms import BookForm
+from .models import Book
+# LibraryProject/bookshelf/views.py
+
+from .forms import ExampleForm
 
 
-def register(request):
-    if request.method == "POST":
-        form = CustomUserCreationForm(request.POST)
+def example_view(request):
+    form = ExampleForm()
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            role = form.cleaned_data.get("role")
-            if role == "creator":
-                creator_group = Group.objects.get(name="Creator")
-                user.groups.add(creator_group)
-            elif role == "reader":
-                creator_group = Group.objects.get(name="Reader")
-                user.groups.add(creator_group)
-            
-            login(request, user)
-            return redirect("list_posts")
-    else:
-        form = CustomUserCreationForm()
-    return render(request, "blog/register.html", {"form": form})
+            # Process the form data
+            pass
+    return render(request, 'bookshelf/form_example.html', {'form': form})
 
+
+# views.py
+
+@permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
-    books = Book.objects.all()
-    return render(request, "blog/book_list.html", {"post": books})
-
-@login_required
-@permission_required("blog.create", raise_exception=True)
-def create_post(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            posts = form.save(commit=False)
-            posts.author = request.user
-            posts.save()
-            return redirect("list_posts")
-        
+    search_query = request.GET.get('q', '')
+    if search_query:
+        books = Book.objects.filter(Q(title__icontains=search_query) | Q(author__icontains=search_query))
     else:
-        form = PostForm()
-    return render(request, "blog/create_post.html", {"form": form})
+        books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
 
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'bookshelf/form_example.html', {'form': form})
+
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def view_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    return render(request, 'bookshelf/view_book.html', {'book': book})
+
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_book(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        author = request.POST.get('author')
+        book = Book.objects.create(title=title, author=author)
+        return redirect('view_book', book_id=book.id)
+    return render(request, 'bookshelf/create_book.html')
+
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'bookshelf/delete_book.html', {'book': book})
